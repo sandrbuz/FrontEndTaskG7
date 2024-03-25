66604ea6b06ed96aa3b74013de199c175bcdd1f4
sandrbuz <sandrbuz@mail.ru>
2024-03-26 00:47:37 +0200

    added plus/minus handlers, and color red

--- src/App.js
+++ src/App.js
@@ -4,18 +4,45 @@ import Controller from './Controller/Controller';
 
 function App() {
 
+  const [state, setState] = useState([
+    { value: 89, name: 'VOLUME', id: 0 },
+    { value: 32, name: 'TREBLE', id: 1 },
+    { value: 50, name: 'MID', id: 2 },
+    { value: 41, name: 'BASS', id: 3 },
+  ]
+  )
+  const handlePlus = (currentId) => {
+    setState(
+      state.map(c => {
+        return {
+          ...c,
+          value: c.id == currentId ? c.value + 1 : c.value
+        }
+      })
+    );
+  }
+  const handleMinus = (currentId) => {
+    setState(
+      state.map(c => {
+        return {
+          ...c,
+          value: c.id == currentId ? c.value - 1 : c.value
+        }
+      })
+    );
+  }
+
+  const Controllers = state.map(controller => <Controller handlePlus={handlePlus} handleMinus={handleMinus} value={controller.value} name={controller.name} key={controller.id} id={controller.id} />);
+  // crypto.randomUUID()
 
   return (
     <div className="App">
       <div className='main'>
         <div className='controllers'>
-          <Controller/>
-          <Controller/>
-          <Controller/>
-          <Controller/>
+          {Controllers}
         </div>
         <form className='form'>
-          <input value="enter control name" />
+          <input onChange={() => { }} value="enter control name" />
           <button>Add</button>
         </form>
       </div>
--- src/Controller/Controller.jsx
+++ src/Controller/Controller.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import s from "./Controller.module.css"
 
 const Controller = (props) => {
+    
+    const isRed = (props.value > 90 && props.name=="VOLUME");
+
     return (
         <div className={s.controller}>
-            <button className={s.btn}>-</button>
-            <div>
-                <div>50</div>
-                <div>name</div>
+            <button onClick={()=> props.handleMinus(props.id)} className={s.btn}>-</button>
+            <div className={isRed ? s.red : undefined}>
+                <div>{props.value}</div>
+                <div>{props.name}</div>
             </div>
-            <button className={s.btn}>+</button>
+            <button onClick={()=> props.handlePlus(props.id)} className={s.btn}>+</button>
         </div>
     )
 }
